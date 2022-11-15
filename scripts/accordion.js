d5b6f4716242e0efb65cbe869a1b84f8897ab0c6
Ivan Klimovskiy <klimoffsky2707@yandex.ru>
2022-11-15 21:59:23 +0300

    Удалить лишние стили и файлы

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -2,7 +2,7 @@ const buttonsOpenAccordeon = document.querySelectorAll('.accordion__button');
 
 
 Array.from(buttonsOpenAccordeon).forEach( (item) => {
-  item.addEventListener("click", function(evt) {
+  item.addEventListener('click', function(evt) {
     evt.target.classList.toggle('accordion__button_open')
     const accordeonContent = item.closest('.accordion__item').lastElementChild;
     if (accordeonContent.style.maxHeight) {
