0c41e8e27f5a932e95c0136e79b4924937277bf4
Ivan Klimovskiy <klimoffsky2707@yandex.ru>
2022-11-16 23:34:19 +0300

    Изменить код для аккордеона

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -5,14 +5,14 @@ Array.from(buttonsOpenAccordeon).forEach( (item) => {
   item.addEventListener('click', function(evt) {
     evt.target.classList.toggle('accordion__button_open')
     const accordeonContent = item.closest('.accordion__item').lastElementChild;
+    
     if (accordeonContent.style.maxHeight) {
       accordeonContent.style.maxHeight = null;
-      accordeonContent.style.paddingTop = '0'
+      accordeonContent.style.marginBottom = '0'
     } else {
       accordeonContent.style.maxHeight = null;
       accordeonContent.style.maxHeight = accordeonContent.scrollHeight + 'px'
-      accordeonContent.style.paddingTop = '7px'
-    }
-    
+      accordeonContent.style.marginBottom = '39px'
+    } 
   })
 })
